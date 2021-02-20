1f9a130fbec765c7b66fb36e64598ae3c4556dbd
Caitlin Tighe <caitighe@gmail.com>
2021-02-20 14:41:07 -0500

    added user criteria and confirmation criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,56 @@
 var generateBtn = document.querySelector("#generate");
-var password = "";
-var passString = [];
 
+var numberSet = [0,1,2,3,4,5,6,7,8,9];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialChar = ["'","!","(",")","?","[","]","`","~",";",":","@","#","$","%","^","&","*","+","="];
 
+var confirmNumbers;
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmSpecialChar;
+var confirmLength;
 
-// Creating Arrays //
+  function userInput () {
+    var confirmLength = parseInt(prompt("Please enter desired password length (8-128), hit OK"));
+ 
+    if (isNaN(confirmLength === true) {
+      alert("You must choose a number 8-128 to continue");
+      return 
+    }
 
-generateBtn.addEventListener("click", generatePassword)
+    else if (confirmLength === null) {
+      alert("You must create a password to continue");
+      return
+    }
 
-// Write password to the #password input
+    else if (confirmLength < 8 || confirmLength > 128) {
+      alert("Please enter a number 8-128");
+      return
+    }
 
-function generatePassword() {
+    confirmNumber = confirm("This password will include numbers");
+    confirmLowerCase = confirm("This password will include lower case letters");
+    confirmUpperCase = confirm("This password will include upper case letters");
+    confirmSpecialChar = confirm("This password will include special characters");
 
-  var numberSet = "0123456789";
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var specialChar = "!()?[]`~;:!@#$%^&*+=";
-
-
-
-
-  var passwordLength = prompt("Please enter desired password length (8-128), hit OK");
-
-  // If user puts in nothing //
-  if (passwordLength === null) {
-    alert("You must create a password to continue");
-    return generatePassword();
+ 
   }
-  // Making sure input is a number value //
-  else if (isNan(passwordLength)) {
-    alert("Please enter a number 8-128");
-    return generatePassword();
+
+  function generatePassword() {
+    var userData = userInput();
+
+    var password = [];
+    var chosenChar = [];
+    var totalChar = [];
   }
 
-  else if (passwordLength < 8 || passwordLength > 128) {
-    alert("Please pick a number 8-128");
-    return generatePassword ();
-   }
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 
-   var numbers = confirm("This password will include numbers");
 
-   if (numbers) {
-
-   }
 
    generateBtn.addEventListener("click", writePassword)
