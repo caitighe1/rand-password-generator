2509bd54c2ea92b37b41937e8b8689abfe077c2e
Caitlin Tighe <caitighe@gmail.com>
2021-02-20 00:06:53 -0500

    fixing the structure of the top of js code

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,5 @@
-// Creating Arrays //
-var numberSet = "0123456789";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialChar = "!()?[]`~;:!@#$%^&*+=";
-
-var passwordLength;
-var confirmUpperCase;
-var confirmLowerCase;
-var confirmNumber;
-var confirmSpecialChar;
-
-
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -26,46 +8,32 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-document.querySelector("#generate").addEventListener("click", writePassword)
+generateBtn.addEventListener("click", writePassword);
+
+// Creating Arrays //
+
+var numberSet = "0123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChar = "!()?[]`~;:!@#$%^&*+=";
+
 
 
 
 
 // Write password to the #password input
+
 function generatePassword() {
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = "";
   var passwordLength = prompt("Please enter desired password length (8-128), hit OK");
 
+  
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("Please enter a number 8-128");
+    alert("Please pick a number 8-128")
+    return;
   }
 
-
-
-
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-document.querySelector() desiredPasswordLength.value;
-
-if (validInput === isNaN) {
-  alert("Please enter a number 8-128");
-}
-else if (validInput < 8 || desiredPasswordLength > 128) {
-  alert("Please enter a number 8-128")
-
-}
-else if (validInput === "")
-alert("Field cannot be empty")
-}
-
+    
+ 
 
