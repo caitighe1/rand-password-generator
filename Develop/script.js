b8d7ad27ddd6d9dfa92ba26575775a38f3471caa
Caitlin Tighe <caitighe@gmail.com>
2021-02-20 13:38:46 -0500

    creating loops

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,47 @@
 var generateBtn = document.querySelector("#generate");
+var password = "";
+var passString = [];
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-generateBtn.addEventListener("click", writePassword);
 
 // Creating Arrays //
 
-var numberSet = "0123456789";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialChar = "!()?[]`~;:!@#$%^&*+=";
-
-
-
-
+generateBtn.addEventListener("click", generatePassword)
 
 // Write password to the #password input
 
 function generatePassword() {
 
-  var password = "";
+  var numberSet = "0123456789";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialChar = "!()?[]`~;:!@#$%^&*+=";
+
+
+
+
   var passwordLength = prompt("Please enter desired password length (8-128), hit OK");
 
-  
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("Please pick a number 8-128")
-    return;
+  // If user puts in nothing //
+  if (passwordLength === null) {
+    alert("You must create a password to continue");
+    return generatePassword();
+  }
+  // Making sure input is a number value //
+  else if (isNan(passwordLength)) {
+    alert("Please enter a number 8-128");
+    return generatePassword();
   }
 
-    
- 
+  else if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please pick a number 8-128");
+    return generatePassword ();
+   }
 
+   var numbers = confirm("This password will include numbers");
+
+   if (numbers) {
+
+   }
+
+   generateBtn.addEventListener("click", writePassword)
