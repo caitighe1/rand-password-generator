119423e225c8369d884e71382e15dcf227975220
Caitlin Tighe <caitighe@gmail.com>
2021-02-19 19:29:12 -0500

    created JS code for first prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+generateBtn.addEventListener("click", generatePassword);
+
+function generatePassword () {
+  var newUser = prompt("Please enter desired password length (8-128), hit OK");
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
